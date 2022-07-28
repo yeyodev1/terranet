62c2794b551a04836c9fd15ce07d3213b85eec56
Diego Reyes <diegorele13@gmail.com>
2022-07-28 15:45:12 -0500

    created: home page

---
+++ Views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="background">
+        <TheHeader />
+        <TheHero />
+    </div>
+</template>
+
+<script></script>
+
+<style scoped>
+.background {
+    background-image: url('@/static/Backgrounds/Background-2.png');
+}
+</style>
