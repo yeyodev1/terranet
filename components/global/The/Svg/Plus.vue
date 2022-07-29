8f612b122ade0bd58bdf869aeb3cf7e2314718cb
Diego Reyes <diegorele13@gmail.com>
2022-07-28 19:05:32 -0500

    add: plus icon svg

---
+++ components/global/The/Svg/Plus.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="w-full">
+        <svg
+            width="8"
+            height="8"
+            viewBox="0 0 8 8"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <g filter="url(#filter0_i_216_1280)">
+                <path
+                    d="M4.78441 4.69526H4.62946V4.85021V7.01948C4.62946 7.10505 4.56009 7.17442 4.47451 7.17442H3.44153C3.35595 7.17442 3.28658 7.10505 3.28658 7.01948V4.85021V4.69526H3.13163H0.962362C0.876786 4.69526 0.807414 4.62589 0.807414 4.54031V3.50732C0.807414 3.42175 0.876786 3.35238 0.962362 3.35238H3.13163H3.28658V3.19743V1.02816C3.28658 0.942582 3.35595 0.87321 3.44153 0.87321H4.47451C4.56009 0.87321 4.62946 0.942582 4.62946 1.02816V3.19743V3.35238H4.78441H6.95368C7.03926 3.35238 7.10863 3.42175 7.10863 3.50732V4.54031C7.10863 4.62589 7.03926 4.69526 6.95368 4.69526H4.78441Z"
+                    stroke="#FAFBFF"
+                    stroke-width="0.309896"
+                />
+            </g>
+            <defs>
+                <filter
+                    id="filter0_i_216_1280"
+                    x="0.34257"
+                    y="0.408366"
+                    width="6.92101"
+                    height="6.92098"
+                    filterUnits="userSpaceOnUse"
+                    color-interpolation-filters="sRGB"
+                >
+                    <feFlood flood-opacity="0" result="BackgroundImageFix" />
+                    <feBlend
+                        mode="normal"
+                        in="SourceGraphic"
+                        in2="BackgroundImageFix"
+                        result="shape"
+                    />
+                    <feColorMatrix
+                        in="SourceAlpha"
+                        type="matrix"
+                        values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+                        result="hardAlpha"
+                    />
+                    <feOffset dx="-0.309896" dy="-0.309896" />
+                    <feGaussianBlur stdDeviation="0.309896" />
+                    <feComposite
+                        in2="hardAlpha"
+                        operator="arithmetic"
+                        k2="-1"
+                        k3="1"
+                    />
+                    <feColorMatrix
+                        type="matrix"
+                        values="0 0 0 0 0.207843 0 0 0 0 0.45098 0 0 0 0 0.898039 0 0 0 0.5 0"
+                    />
+                    <feBlend
+                        mode="normal"
+                        in2="shape"
+                        result="effect1_innerShadow_216_1280"
+                    />
+                </filter>
+            </defs>
+        </svg>
+    </div>
+</template>
