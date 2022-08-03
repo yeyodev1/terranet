9352ff11c75501b929f15fe5f5a2a42c78e4c732
Diego Reyes <diegorele13@gmail.com>
2022-08-03 17:17:37 -0500

    remove: styles with background to each component

--- Views/Home.vue
+++ Views/Home.vue
@@ -5,11 +5,12 @@
             <HomeHero />
         </div>
 
-        <PhoneCallToAction class="background-phoneCallToAction" />
-        <div class="background">
-            <OurPlanes />
-        </div>
+        <PhoneCallToAction />
+
+        <OurPlanes />
+
         <Benefits />
+        <PaymentsMethod />
     </div>
 </template>
 
@@ -17,11 +18,13 @@
 import PhoneCallToAction from '../components/Home/PhoneCallToAction.vue'
 import OurPlanes from '~/components/Home/OurPlanes.vue'
 import Benefits from '~/components/Home/Benefits.vue'
+import PaymentsMethod from '~/components/Home/PaymentsMethod.vue'
 export default {
     components: {
         PhoneCallToAction,
         OurPlanes,
         Benefits,
+        PaymentsMethod,
     },
 }
 </script>
@@ -29,12 +32,6 @@ export default {
 <style scoped>
 .background {
     background-image: url('@/static/Backgrounds/Background-2.png');
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-}
-.background-phoneCallToAction {
-    background-image: url('@/static/Backgrounds/Background-3.png');
     background-repeat: no-repeat;
     background-position: center;
     background-size: cover;
