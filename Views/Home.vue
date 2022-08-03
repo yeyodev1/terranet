c17dbff3bef10dea0d31a3b4f39e505ccc3aeb8b
Diego Reyes <diegorele13@gmail.com>
2022-08-02 20:24:23 -0500

    add: benefits

--- Views/Home.vue
+++ Views/Home.vue
@@ -9,16 +9,19 @@
         <div class="background">
             <OurPlanes />
         </div>
+        <Benefits />
     </div>
 </template>
 
 <script>
 import PhoneCallToAction from '../components/Home/PhoneCallToAction.vue'
 import OurPlanes from '~/components/Home/OurPlanes.vue'
+import Benefits from '~/components/Home/Benefits.vue'
 export default {
     components: {
         PhoneCallToAction,
         OurPlanes,
+        Benefits,
     },
 }
 </script>
