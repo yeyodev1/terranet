2c45b9545e2fe2aa559f2e33db089ec23414e1be
Diego Reyes <diegorele13@gmail.com>
2022-08-02 19:42:48 -0500

    add: new background

--- Views/Home.vue
+++ Views/Home.vue
@@ -6,14 +6,19 @@
         </div>
 
         <PhoneCallToAction class="background-phoneCallToAction" />
+        <div class="background">
+            <OurPlanes />
+        </div>
     </div>
 </template>
 
 <script>
 import PhoneCallToAction from '../components/Home/PhoneCallToAction.vue'
+import OurPlanes from '~/components/Home/OurPlanes.vue'
 export default {
     components: {
         PhoneCallToAction,
+        OurPlanes,
     },
 }
 </script>
