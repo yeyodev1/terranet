d97806a13b8f2de34dbabf6cd82af7a1a7464982
Diego Reyes <diegorele13@gmail.com>
2022-08-04 15:36:17 -0500

    created: footer

---
+++ components/global/The/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-full bg-white"></div>
+</template>
+
+<script>
+import Icons from '@/components/global/Icons.vue'
+export default {
+    components: {
+        Icons,
+    },
+    data: () => ({
+        phone: 'phone',
+    }),
+}
+</script>
