d10386fec6c630698636e87fcca27b998a5fdc3c
Diego Reyes <diegorele13@gmail.com>
2022-08-04 15:35:56 -0500

    add: footer component

--- Views/Home.vue
+++ Views/Home.vue
@@ -12,6 +12,7 @@
         <Benefits />
         <PaymentsMethod />
         <PhoneCallToAction />
+        <Footer />
     </div>
 </template>
 
@@ -20,12 +21,14 @@ import PhoneCallToAction from '../components/Home/PhoneCallToAction.vue'
 import OurPlanes from '~/components/Home/OurPlanes.vue'
 import Benefits from '~/components/Home/Benefits.vue'
 import PaymentsMethod from '~/components/Home/PaymentsMethod.vue'
+import Footer from '~/components/global/The/Footer.vue'
 export default {
     components: {
         PhoneCallToAction,
         OurPlanes,
         Benefits,
         PaymentsMethod,
+        Footer,
     },
 }
 </script>
