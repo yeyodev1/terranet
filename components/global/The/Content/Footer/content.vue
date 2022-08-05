e4367d1b366ff22107dabd1a3b8304f1dbcdd780
Diego Reyes <diegorele13@gmail.com>
2022-08-05 16:39:01 -0500

    add: file with content footer

---
+++ components/global/The/Content/Footer/content.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-full flex flex-col justify-start items-center">
+        <p class="w-full text-xl font-bold text-blue">
+            {{ linkSection.title }}
+        </p>
+        <div class="w-full flex flex-col">
+            <a
+                v-for="(link, index) in linkSection.links"
+                :key="index"
+                target="_blank"
+                :href="link.url"
+                class="w-full mt-3 text-xl text-black transition-border"
+            >
+                {{ link.name }}
+            </a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        linkSection: {
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
