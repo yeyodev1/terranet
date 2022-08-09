ded471089f03e89f3972c50db95eb940e9f6ed06
Diego Reyes <diegorele13@gmail.com>
2022-08-09 15:41:59 -0500

    done: second question plan card

---
+++ components/Cards/SecondQuestionPlan.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="w-full mt-8 border-2 border-white rounded-2xl">
+        <div class="flex items-start justify-start pt-8 px-7">
+            <p
+                class="pr-4 text-5xl font-bold text-center text-white font-principal"
+            >
+                2
+            </p>
+            <p class="text-base font-bold text-white text-start font-principal">
+                {{ quiz.question }}
+            </p>
+        </div>
+        <div class="flex flex-col items-center justify-center pt-20 pb-16">
+            <button
+                class="w-4/5 mt-5 font-semibold text-white border rounded-md border-yellow"
+                :class="{ 'bg-yellow': selected(option.option) }"
+                v-for="(option, index) in quiz.options"
+                :key="index"
+                @click.prevent="getItem(option.option)"
+            >
+                {{ option.option }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        quiz: {
+            type: Object,
+            required: true,
+        },
+    },
+    data: () => ({
+        selectedItem: '',
+    }),
+    methods: {
+        getItem(item) {
+            if (this.selectedItem === item) {
+                this.selectedItem = ''
+            } else {
+                this.selectedItem = item
+            }
+        },
+        selected(item) {
+            if (this.selectedItem === item) {
+                return true
+            }
+            return false
+        },
+    },
+}
+</script>
