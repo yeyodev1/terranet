08a88000ecf3985ea44874e0ce60e4b14d610b4a
Diego Reyes <diegorele13@gmail.com>
2022-08-09 18:58:21 -0500

    created: corporationd devices

---
+++ components/Plans/CorporationDevices.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p
+            class="px-5 pb-10 text-white font-principal font-bold text-2xl text-center pt-11"
+        >
+            ¿Cuántos dispositivos conectarás a la red?
+        </p>
+        <div class="w-4/5">
+            <div>
+                <div></div>
+                <p></p>
+            </div>
+            <div>
+                <div></div>
+                <p></p>
+            </div>
+            <div>
+                <div></div>
+                <p></p>
+            </div>
+            <div>
+                <div></div>
+                <p></p>
+            </div>
+        </div>
+    </div>
+</template>
