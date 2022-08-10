9e069a727a266092d5172c61f39e0ab79e6b503d
Diego Reyes <diegorele13@gmail.com>
2022-08-10 18:05:49 -0500

    done: corporation devices

--- components/Plans/CorporationDevices.vue
+++ components/Plans/CorporationDevices.vue
@@ -71,6 +71,19 @@
                 />
             </div>
         </div>
+        <div class="flex items-center justify-center w-4/5 mx-auto mt-14">
+            <input type="checkbox" class="w-5 h-5" />
+            <p class="pl-3 text-xs font-principal text-grey">
+                Acepto las políticas de privacidad
+            </p>
+        </div>
+        <div class="flex items-center justify-center w-full">
+            <button
+                class="flex items-center justify-center px-6 py-3 mb-6 text-center text-white border rounded-md font-principal mt-9 border-yellow sm:mb-12"
+            >
+                Enviar
+            </button>
+        </div>
     </div>
 </template>
 
