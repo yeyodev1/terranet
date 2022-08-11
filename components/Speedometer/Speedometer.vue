ea41a88be53712efbe980ec8b6d100f6aaecc8cf
Diego Reyes <diegorele13@gmail.com>
2022-08-10 19:23:36 -0500

    created: button of speedometer

---
+++ components/Speedometer/Speedometer.vue
@@ -0,0 +1,15 @@
+<template>
+    <div
+        class="flex items-center justify-center w-4/5 max-w-6xl mx-auto mt-8 border rounded-md border-lightBlue sm:mt-12"
+    >
+        <div
+            class="flex items-center justify-center px-5 border rounded-full my-9 w-80 h-80 border-lightBlue py-28"
+        >
+            <button
+                class="text-4xl text-center text-white uppercase sm:w-80 sm:h-80 sm:text-5xl font-principal"
+            >
+                Comenzar
+            </button>
+        </div>
+    </div>
+</template>
