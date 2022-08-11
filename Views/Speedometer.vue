79296af37dda069591c6bd511aa09a6f733f2fc0
Diego Reyes <diegorele13@gmail.com>
2022-08-10 19:22:58 -0500

    created: speedometer view

---
+++ Views/Speedometer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="w-full background">
+        <Header />
+        <div class="w-4/5 mx-auto sm:pt-20">
+            <p
+                class="pt-12 text-2xl font-bold text-center text-white font-principal sm:text-5xl"
+            >
+                Test de velocidad
+            </p>
+        </div>
+        <Speedometer />
+        <Tips />
+    </div>
+</template>
+
+<script>
+import Header from '~/components/global/The/Header.vue'
+import Speedometer from '~/components/Speedometer/Speedometer.vue'
+import Tips from '../components/Speedometer/Tips.vue'
+
+export default {
+    components: { Header, Speedometer, Tips },
+}
+</script>
+
+<style scoped>
+.background {
+    background-image: url('@/static/Backgrounds/Background-2.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+}
+</style>
