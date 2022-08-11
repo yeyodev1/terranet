4e7e21fe669acccb334f8794360f12d49e4ab2c4
Diego Reyes <diegorele13@gmail.com>
2022-08-10 19:52:46 -0500

    add: card of paymethods

---
+++ components/Cards/PayMethods.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex items-center justify-center w-full">
+        <div class="w-4/5 mt-8 max-w-7xl background">
+            <div class="w-24 h-14">
+                <!-- aqui va el logo el banco pichincha -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.background {
+    background: linear-gradient(
+        155.02deg,
+        rgba(250, 251, 255, 0.4) 0.55%,
+        rgba(250, 251, 255, 0.190022) 20.21%,
+        rgba(250, 251, 255, 0.124771) 34.02%,
+        rgba(250, 251, 255, 0.0654234) 44.23%,
+        rgba(250, 251, 255, 0.02) 75.4%
+    );
+    border-radius: 8px;
+}
+</style>
