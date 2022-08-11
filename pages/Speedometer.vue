e2149421b5059a227a35babd27b0b0c93ecd4500
Diego Reyes <diegorele13@gmail.com>
2022-08-10 19:22:42 -0500

    created: page of speedmeter

---
+++ pages/Speedometer.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <Speedometer />
+    </div>
+</template>
+
+<script>
+import Speedometer from '~/Views/Speedometer.vue'
+export default {
+    components: { Speedometer },
+}
+</script>
