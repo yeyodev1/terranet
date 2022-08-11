21dbe32fc40bfc28c9df5b599772d397123845ab
Diego Reyes <diegorele13@gmail.com>
2022-08-10 19:23:26 -0500

    done: speedometer tips

---
+++ components/Speedometer/Tips.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="w-full">
+        <div class="w-4/5 max-w-6xl mx-auto mt-7">
+            <p class="text-base font-bold text-white font-principal">
+                Tips para antes de empezar:
+            </p>
+            <div class="w-4/5">
+                <p class="text-base text-white font-principal">
+                    • Selecciona el servidor/Host de
+                    <strong>TERRANET</strong> para un mejor resultado.
+                </p>
+                <p class="text-base text-white font-principal">
+                    • Conecta tu laptop o PC directo al nuestro equipo
+                    (modem/router).
+                </p>
+                <p class="text-base text-white font-principal">
+                    • Asegúrate que ningún otro equipo esté conectado a la red
+                    wifi.
+                </p>
+                <p class="text-base text-white font-principal">
+                    • Deja habilitada la pestaña del
+                    <strong>Test de velocidad</strong> y cierra todas las demás
+                    pestañas de navegación de tu equipo
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
