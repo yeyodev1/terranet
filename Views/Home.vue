75d3c64c5956045fd97b7c6f091fa58035340808
Diego Reyes <diegorele13@gmail.com>
2022-08-13 17:02:35 -0500

    add: supportbutton component

--- Views/Home.vue
+++ Views/Home.vue
@@ -1,15 +1,16 @@
 <template>
-  <div>
-    <HomeHero />
+    <div>
+        <SupportButton />
+        <HomeHero />
 
-    <PhoneCallToAction />
+        <PhoneCallToAction />
 
-    <OurPlanes />
+        <OurPlanes />
 
-    <Benefits />
-    <PaymentsMethod />
-    <PhoneCallToAction />
-  </div>
+        <Benefits />
+        <PaymentsMethod />
+        <PhoneCallToAction />
+    </div>
 </template>
 
 <script>
@@ -17,12 +18,14 @@ import PhoneCallToAction from '../components/Home/PhoneCallToAction.vue'
 import OurPlanes from '~/components/Home/OurPlanes.vue'
 import Benefits from '~/components/Home/Benefits.vue'
 import PaymentsMethod from '~/components/Home/PaymentsMethod.vue'
+import SupportButton from '~/components/Home/SupportButton.vue'
 export default {
-  components: {
-    PhoneCallToAction,
-    OurPlanes,
-    Benefits,
-    PaymentsMethod,
-  },
+    components: {
+        PhoneCallToAction,
+        OurPlanes,
+        Benefits,
+        PaymentsMethod,
+        SupportButton,
+    },
 }
 </script>
