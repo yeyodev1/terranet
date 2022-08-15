32132df24214391527de7dc6350d2087d86a3134
Diego Reyes <diegorele13@gmail.com>
2022-08-15 15:35:11 -0500

    add: links

--- components/Home/SupportButton.vue
+++ components/Home/SupportButton.vue
@@ -6,15 +6,27 @@
             <Icons class="w-5 h-5" :name="message" />
         </div>
         <div class="message-options">
-            <div class="p-4 rounded-lg bg-yellow text-[#335BDE]">
-                <Icons class="w-5 h-5" :name="messenger" />
-            </div>
-            <div class="p-4 rounded-lg bg-yellow my-4 text-[#AA0047]">
-                <Icons class="w-5 h-5" :name="instagram" />
-            </div>
-            <div class="p-4 rounded-lg bg-yellow text-[#00A441]">
-                <Icons class="w-5 h-5" :name="whatsapp" />
-            </div>
+            <a
+                href="https://www.facebook.com/messages/t/111849641352264"
+                target="_blank"
+            >
+                <div class="p-4 rounded-lg bg-yellow text-[#335BDE]">
+                    <Icons class="w-5 h-5" :name="messenger" />
+                </div>
+            </a>
+            <a href="https://www.instagram.com/terranet.ec/" target="_blank">
+                <div class="p-4 rounded-lg bg-yellow my-4 text-[#AA0047]">
+                    <Icons class="w-5 h-5" :name="instagram" />
+                </div>
+            </a>
+            <a
+                href="https://api.whatsapp.com/send?phone=593967377000&text=%C2%A1Hola!%20Vi%20tu%20perfil%[…]20y%20estoy%20interesado%20en%20un%20plan%20de%20internet%20"
+                target="_blank"
+            >
+                <div class="p-4 rounded-lg bg-yellow text-[#00A441]">
+                    <Icons class="w-5 h-5" :name="whatsapp" />
+                </div>
+            </a>
         </div>
     </div>
 </template>
