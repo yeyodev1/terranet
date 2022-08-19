88039958eb6ce3c00f115286a295fb7076f8072b
Diego Reyes <diegorele13@gmail.com>
2022-08-18 19:05:25 -0500

    add: component with the components

---
+++ pages/hireservice/Components/hireserviceview.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Form />
+        <Benefits />
+        <PhoneCallToAction />
+    </div>
+</template>
+
+<script>
+import Benefits from '~/components/Home/Benefits.vue'
+import PhoneCallToAction from '~/components/Home/PhoneCallToAction.vue'
+import Form from '~/components/HireService/form.vue'
+export default {
+    components: {
+        Benefits,
+        PhoneCallToAction,
+        Form,
+    },
+}
+</script>
