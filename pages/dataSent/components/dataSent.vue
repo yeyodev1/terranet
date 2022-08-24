1cec10139934cc6dbb532c72e751059f121b8c3b
Diego Reyes <diegorele13@gmail.com>
2022-08-24 17:58:56 -0500

    add: all components necessary

---
+++ pages/dataSent/components/dataSent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full">
+        <HeroData />
+        <div>
+            <Footer />
+        </div>
+    </div>
+</template>
+
+<script>
+import Footer from '~/components/global/The/Footer.vue'
+import HeroData from '~/components/dataSent/heroData.vue'
+export default {
+    components: { Footer, HeroData },
+}
+</script>
