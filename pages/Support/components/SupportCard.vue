2a2a626c94cd1d42c96b5cffeb9349a8bad0c42c
Diego Reyes <diegorele13@gmail.com>
2022-08-24 18:31:17 -0500

    nothing

--- pages/Support/components/SupportCard.vue
+++ pages/Support/components/SupportCard.vue
@@ -1,36 +1,27 @@
 <template>
-  <button
-    class="
-      sm:w-72 sm:h-32
-      w-32
-      h-32
-      flex flex-col
-      border-2 border-lightBlue
-      justify-center
-      items-center
-      rounded
-    "
-  >
-    <div class="w-9 h-11">
-      <Icons :name="icon" class="text-yellow" />
-    </div>
-    <p class="text-white text-center mt-3">
-      {{ name }}
-    </p>
-  </button>
+    <button
+        class="flex flex-col items-center justify-center w-32 h-32 border-2 rounded sm:w-72 sm:h-32 border-lightBlue"
+    >
+        <div class="w-9 h-11">
+            <Icons :name="icon" class="text-yellow" />
+        </div>
+        <p class="mt-3 text-center text-white">
+            {{ name }}
+        </p>
+    </button>
 </template>
 
 <script>
 export default {
-  props: {
-    icon: {
-      type: String,
-      required: true,
+    props: {
+        icon: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
     },
-    name: {
-      type: String,
-      required: true,
-    },
-  },
 }
 </script>
--- pages/Support/index.vue
+++ pages/Support/index.vue
@@ -1,60 +1,49 @@
 <template>
-  <div class="mx-auto w-full">
-    <div
-      class="
-        w-full
-        flex flex-col
-        justify-center
-        items-center
-        xl:mt-24 xl:mb-20
-        md:mt-16 md:mb-12
-        my-7
-      "
-    >
-      <p class="text-base font-medium text-center text-white sm:text-3xl">
-        Atención al cliente
-      </p>
-      <p class="text-2xl font-bold text-center text-white sm:text-5xl my-3">
-        ¿En qué podemos ayudarte?
-      </p>
-      <p class="text-base font-medium text-center text-white sm:text-2xl">
-        Selecciona la opción que necesitas
-      </p>
-    </div>
-    <div
-      class="
-        w-full
-        mx-auto
-        max-w-4xl
-        flex flex-wrap
-        justify-center
-        items-center
-      "
-    >
-      <div
-        v-for="(item, index) in support"
-        :key="index"
-        class="w-1/2 lg:w-1/3 flex justify-center items-center p-5"
-      >
-        <support-card :icon="item.icon" :name="item.name" />
-      </div>
-    </div>
-    <div class="w-full mx-auto my-20">
-      <p class="text-2xl lg:text-4xl font-semibold text-center text-white">
-        Alguien ya hizo esa pregunta:
-      </p>
-      <div class="flex justify-center items-center mt-3">
-        <p class="text-base font-medium text-center text-white">
-          Consulta las preguntas frecuentas aquí
-        </p>
-        <div class="w-6 h-6 flex justify-center items-center">
-          <icons name="arrowRight" class="text-yellow" />
+    <div class="w-full mx-auto">
+        <div
+            class="flex flex-col items-center justify-center w-full xl:mt-24 xl:mb-20 md:mt-16 md:mb-12 my-7"
+        >
+            <p class="text-base font-medium text-center text-white sm:text-3xl">
+                Atención al cliente
+            </p>
+            <p
+                class="my-3 text-2xl font-bold text-center text-white sm:text-5xl"
+            >
+                ¿En qué podemos ayudarte?
+            </p>
+            <p class="text-base font-medium text-center text-white sm:text-2xl">
+                Selecciona la opción que necesitas
+            </p>
         </div>
-      </div>
+        <div
+            class="flex flex-wrap items-center justify-center w-full max-w-4xl mx-auto"
+        >
+            <div
+                v-for="(item, index) in support"
+                :key="index"
+                class="flex items-center justify-center w-1/2 p-5 lg:w-1/3"
+            >
+                <support-card :icon="item.icon" :name="item.name" />
+            </div>
+        </div>
+        <div class="w-full mx-auto my-20">
+            <p
+                class="text-2xl font-semibold text-center text-white lg:text-4xl"
+            >
+                Alguien ya hizo esa pregunta:
+            </p>
+            <div class="flex items-center justify-center mt-3">
+                <p class="text-base font-medium text-center text-white">
+                    Consulta las preguntas frecuentas aquí
+                </p>
+                <div class="flex items-center justify-center w-6 h-6">
+                    <icons name="arrowRight" class="text-yellow" />
+                </div>
+            </div>
+        </div>
+        <HomeBenefits />
+        <HomePhoneCallToAction />
     </div>
-    <HomeBenefits />
-    <HomePhoneCallToAction />
-  </div>
 </template>
 
 <script>
@@ -62,49 +51,49 @@ import Icons from '~/components/global/Icons.vue'
 import SupportCard from './components/SupportCard.vue'
 
 export default {
-  components: {
-    SupportCard,
-    Icons,
-  },
-  data: () => ({
-    support: [
-      {
-        icon: 'bank',
-        name: 'Medio de pago',
-      },
-      {
-        icon: 'microphone',
-        name: 'Soporte técnico',
-      },
-      {
-        icon: 'card',
-        name: 'Pago en línea',
-      },
-      {
-        icon: 'truck',
-        name: 'Solicitar traslado',
-      },
-      {
-        icon: 'changeUser',
-        name: 'Cambio de titular',
-      },
-      {
-        icon: 'changeFile',
-        name: 'Actualizar Datos',
-      },
-      {
-        icon: 'accountVoice',
-        name: 'Tú opinión',
-      },
-      {
-        icon: 'clockFast',
-        name: 'Velocímetro',
-      },
-      {
-        icon: 'statistic',
-        name: 'Consultar capacidad',
-      },
-    ],
-  }),
+    components: {
+        SupportCard,
+        Icons,
+    },
+    data: () => ({
+        support: [
+            {
+                icon: 'bank',
+                name: 'Medio de pago',
+            },
+            {
+                icon: 'microphone',
+                name: 'Soporte técnico',
+            },
+            {
+                icon: 'card',
+                name: 'Pago en línea',
+            },
+            {
+                icon: 'truck',
+                name: 'Solicitar traslado',
+            },
+            {
+                icon: 'changeUser',
+                name: 'Cambio de titular',
+            },
+            {
+                icon: 'changeFile',
+                name: 'Actualizar Datos',
+            },
+            {
+                icon: 'accountVoice',
+                name: 'Tú opinión',
+            },
+            {
+                icon: 'clockFast',
+                name: 'Velocímetro',
+            },
+            {
+                icon: 'statistic',
+                name: 'Consultar capacidad',
+            },
+        ],
+    }),
 }
 </script>
