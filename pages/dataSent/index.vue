01529cc8ce86a336d9bb9ac7a3f539c170160550
Diego Reyes <diegorele13@gmail.com>
2022-08-24 17:58:19 -0500

    done: page

---
+++ pages/dataSent/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full">
+        <DataSent />
+    </div>
+</template>
+
+<script>
+import DataSent from './components/dataSent.vue'
+export default {
+    components: { DataSent },
+}
+</script>
