df1622b38f926c2fce8e95ea384dd9518029fdd7
Diego Reyes <diegorele13@gmail.com>
2022-08-25 16:16:02 -0500

    done: hero of data sent

--- components/dataSent/heroData.vue
+++ components/dataSent/heroData.vue
@@ -3,7 +3,7 @@
         class="flex items-center justify-center w-full py-12 sm:px-7 md:py-40 lg:px-14"
     >
         <div
-            class="flex flex-wrap items-center justify-center w-full md:items-start max-w-7xl"
+            class="flex flex-wrap items-center justify-center w-full md:justify-start md:items-start max-w-7xl"
         >
             <div class="w-4/5 md:w-2/3">
                 <p
@@ -15,6 +15,17 @@
                 <p class="mt-3 text-base text-white font-principal">
                     En breve un asesor Terranet se comunicará contigo
                 </p>
+                <div
+                    class="items-center justify-center w-full md:justify-start md:w-1/4 hidden md:flex mt-12"
+                >
+                    <nuxt-link to="/hireservice">
+                        <button
+                            class="px-6 py-3 sm:text-base text-xs font-bold text-white border rounded-md hover:bg-yellow border-yellow hover:text-black"
+                        >
+                            Volver
+                        </button>
+                    </nuxt-link>
+                </div>
             </div>
             <div
                 class="flex items-center justify-center w-4/5 mt-12 mb-12 md:justify-start md:w-1/3"
@@ -25,7 +36,9 @@
                     <ThanksLaptop class="hidden lg:flex" />
                 </div>
             </div>
-            <div class="flex items-center justify-center w-full">
+            <div
+                class="flex items-center justify-center w-full md:justify-start md:w-1/4 md:hidden"
+            >
                 <nuxt-link to="/hireservice">
                     <button
                         class="px-6 py-3 text-xs font-bold text-white border rounded-md hover:bg-yellow border-yellow hover:text-black"
