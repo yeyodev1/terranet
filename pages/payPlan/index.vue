b66e326998b7417d6de2ed7ee694e79fdcf87d45
Diego Reyes <diegorele13@gmail.com>
2022-08-29 17:50:32 -0500

    index of the page

---
+++ pages/payPlan/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full">
+        <PayToPlan />
+    </div>
+</template>
+
+<script>
+import PayToPlan from './components/paytoplan.vue'
+export default {
+    components: { PayToPlan },
+}
+</script>
