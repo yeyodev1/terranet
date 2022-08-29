d0545691e7fc44902936dec89eb3d4bfbd0b3cf9
Diego Reyes <diegorele13@gmail.com>
2022-08-29 17:50:24 -0500

    add: components to the page

---
+++ pages/payPlan/components/paytoplan.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full">
+        <Pay />
+        <Benefits />
+        <Footer />
+    </div>
+</template>
+
+<script>
+import Benefits from '~/components/Home/Benefits.vue'
+import Footer from '~/components/global/The/Footer.vue'
+import Pay from '~/components/PayPlan/pay.vue'
+export default {
+    components: { Benefits, Footer, Pay },
+}
+</script>
