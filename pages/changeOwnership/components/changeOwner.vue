e614c0c1ce9f85ac90a088a4e53b521726d62286
Diego Reyes <diegorele13@gmail.com>
2022-09-01 15:45:07 -0500

    done: view to change owner

---
+++ pages/changeOwnership/components/changeOwner.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full">
+        <ChangeOwner />
+        <Benefits />
+        <PhoneCallToAction />
+    </div>
+</template>
+
+<script>
+import Benefits from '~/components/Home/Benefits.vue'
+import PhoneCallToAction from '~/components/Home/PhoneCallToAction.vue'
+import ChangeOwner from '~/components/Forms/changeOwner.vue'
+export default {
+    components: { Benefits, PhoneCallToAction, ChangeOwner },
+}
+</script>
---
+++ pages/changeOwnership/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full">
+        <change-owner />
+    </div>
+</template>
+
+<script>
+import ChangeOwner from './components/changeOwner.vue'
+export default {
+    components: { ChangeOwner },
+}
+</script>
