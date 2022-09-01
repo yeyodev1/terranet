6dcc888eda52f9c67f9f7555fbfbe2df5f38a535
Diego Reyes <diegorele13@gmail.com>
2022-09-01 18:43:34 -0500

    done: page

---
+++ pages/yourOpinion/components/opinion.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full">
+        <UserOpinion />
+        <Benefits />
+        <PhoneCallToAction />
+    </div>
+</template>
+
+<script>
+import Benefits from '~/components/Home/Benefits.vue'
+import PhoneCallToAction from '~/components/Home/PhoneCallToAction.vue'
+import UserOpinion from '~/components/Forms/userOpinion.vue'
+export default {
+    components: { Benefits, PhoneCallToAction, UserOpinion },
+}
+</script>
---
+++ pages/yourOpinion/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full">
+        <Opinion />
+    </div>
+</template>
+
+<script>
+import Opinion from './components/opinion.vue'
+export default {
+    components: { Opinion },
+}
+</script>
