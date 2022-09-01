bd496b30bcb9c8bdcac090c55c2772e996f19c48
Diego Reyes <diegorele13@gmail.com>
2022-09-01 15:50:31 -0500

    done: the index view

---
+++ pages/updateData/components/update.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="w-full">
+        <Benefits />
+        <PhoneCallToAction />
+    </div>
+</template>
+
+<script>
+import Benefits from '~/components/Home/Benefits.vue'
+import PhoneCallToAction from '~/components/Home/PhoneCallToAction.vue'
+export default {
+    components: { Benefits, PhoneCallToAction },
+}
+</script>
---
+++ pages/updateData/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full">
+        <Update />
+    </div>
+</template>
+
+<script>
+import Update from './components/update.vue'
+export default {
+    components: { Update },
+}
+</script>
