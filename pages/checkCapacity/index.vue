5cb550375397a778cefc628a33436030295d8e6e
Diego Reyes <diegorele13@gmail.com>
2022-09-01 19:11:17 -0500

    create: index

---
+++ pages/checkCapacity/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full">
+        <Capacity />
+    </div>
+</template>
+
+<script>
+import Capacity from './components/capacity.vue'
+export default {
+    components: { Capacity },
+}
+</script>
