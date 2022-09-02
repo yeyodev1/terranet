b8006f89897b34815d2ebd46f64771f644334276
Diego Reyes <diegorele13@gmail.com>
2022-09-01 19:11:05 -0500

    add: component with components

---
+++ pages/checkCapacity/components/capacity.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-full">
+        <Capacity />
+        <Benefits />
+        <PhoneCallToAction />
+    </div>
+</template>
+
+<script>
+import Benefits from '~/components/Home/Benefits.vue'
+import PhoneCallToAction from '~/components/Home/PhoneCallToAction.vue'
+import Capacity from '~/components/capacity/capacity.vue'
+export default {
+    components: { Benefits, PhoneCallToAction, Capacity },
+}
+</script>
