155b42b82799f0ff6505d62dd887c065a20fc085
Diego Reyes <diegorele13@gmail.com>
2022-09-01 19:11:27 -0500

    done: component with image

---
+++ components/capacity/capacity.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-full pt-8 pb-8 sm:pt-16 sm:pb-12">
+        <p
+            class="mx-auto text-2xl font-bold text-center text-white font-principal sm:text-5xl max-w-7xl"
+        >
+            Consultar Capacidad
+        </p>
+        <div
+            class="flex w-4/5 max-w-3xl mx-auto mt-6 border rounded-md md:mt-16 border-lightBlue"
+        >
+            <div class="w-full p-6">
+                <img
+                    src="@/static/Images/testImage.png"
+                    class="object-cover w-full"
+                    alt=""
+                />
+            </div>
+        </div>
+    </div>
+</template>
