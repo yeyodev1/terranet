dde928d11cb4549f1a0672583e63f6b457e52f8b
Luis Reyes <lualreye@gmail.com>
2022-09-08 17:50:30 -0500

    feat: layout for app and menu component

---
+++ layouts/app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full bg-appBackground">
+    <AppHeader />
+    <div class="w-full min-h-screen flex justify-center items-center">
+      <AppNavbar />
+      <nuxt class="flex-grow" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ pages/admin/NewPlans.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>hola desde los planes</div>
+</template>
+
+<script>
+export default {
+  layout: 'app'
+}
+</script>
