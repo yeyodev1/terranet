b7cf0e2b623b4687431fd22965c79af43b5441a7
Luis Reyes <lualreye@gmail.com>
2022-09-09 04:58:16 -0500

    feat: ready to post data to jobvacancy

--- pages/admin/WorkWithUs/index.vue
+++ pages/admin/WorkWithUs/index.vue
@@ -17,10 +17,57 @@
           </div>
         </button>
         <div v-if="isOpen" class="w-full flex flex-col justify-start mt-6">
-          
+          <div class="w-full flex flex-wrap justify-between items-center pl-1">
+            <div class="w-full flex justify-start items-start">
+              <label class="text-white font-primary text-lg mr-1">Título: </label>
+              <input 
+                v-model="jobTitle" 
+                placeholder="Ingresa el nombre"
+                type="text"
+                class="w-full text-white outline-none border-b border-lightBlue px-1 bg-appBackground">
+            </div>
+            <div class="w-full lg:w-1/2 flex flex-col justify-start items-start mt-4">
+              <label class="text-white font-primary text-lg mr-1">Descripción: </label>
+              <textarea 
+                v-model="jobDescription" 
+                placeholder="Descripcion"
+                type="text"
+                class="w-full lg:w-3/4 text-white outline-none border-b border-lightBlue px-1 bg-appBackground" />
+            </div>
+            <div class="w-full lg:w-1/2 flex flex-col justify-start items-start mt-4">
+              <label class="text-white font-primary text-lg mr-1">Requesitos: </label>
+              <input
+                v-model="jobRequirements"
+                @keyup.enter="getRequirement" 
+                placeholder="Requisito"
+                type="text"
+                class="w-full lg:w-3/4 text-white outline-none border-b border-lightBlue px-1 bg-appBackground mt-6" />
+            </div>
+            <div class="w-full flex justify-start items-baseline mt-3">
+              <p class="text-white mr-2">
+                Requisitos:
+              </p>
+              <div v-if="requirements.length" class="w-full flex flex-wrap mt-3">
+                <div
+                  v-for="(requirement, i) in requirements"
+                  :key="i"
+                  class="flex items-center bg-grey px-1 rounded-full mr-2 mt-2">
+                  <span
+                    class="text-appBackground mr-1"
+                    >{{ requirement }}
+                  </span>
+                  <button
+                    @click="removeRequirement(i)"
+                    class="rounded-full bg-red w-3 h-3">
+                    <icons name="close" class="text-white" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
           <button
-            @click="savePlan"
-            class="w-28 rounded-lg flex justify-center items-center text-appBackground py-1 mt-4"
+            @click="saveVacancy"
+            class="w-28 rounded-lg flex justify-center items-center text-appBackground py-1 mt-6"
             :class="{ 'bg-yellow': isFormValid, 'bg-grey': !isFormValid }">
             Guardar
           </button>
@@ -45,9 +92,10 @@ export default {
   },
   data: () => ({
     isOpen: false,
-    namePlan: '',
-    pricePlan: '',
-    speedPlan: ''
+    jobTitle: '',
+    jobDescription: '',
+    jobRequirements: '',
+    requirements: [],
   }),
    computed: {
     arrowDisplay() {
@@ -61,15 +109,29 @@ export default {
     showCard() {
       this.isOpen = !this.isOpen
     },
-    async savePlan() {
-      const plan = {
-        planName: this.namePlan,
-        price: Number(this.pricePlan),
-        speed: Number(this.speedPlan)
+    resetValues() {
+      this.jobTitle = ''
+      this.jobDescription = ''
+      this.jobRequirements = ''
+      this.requirements = ''
+    },
+    getRequirement() {
+      this.requirements.push(this.jobRequirements)
+      this.jobRequirements = ''
+    },
+    removeRequirement(i) {
+      this.requirements.splice(i, 1)
+    },
+    async saveVacancy() {
+      const vacancy = {
+        jobVacancy: this.jobTitle,
+        jobDescription: this.jobDescription,
+        jobRequirements: this.jobRequirements
       }
-      console.log(plan)
-      const response = await axios.post(`${process.env.NUXT_API}api/plansBoard`, plan)
+      console.log(vacancy)
+      const response = await axios.post(`${process.env.NUXT_API}api/jobVacs`, vacancy)
       console.log(response)
+      this.resetValues()
     }
   }
 }
