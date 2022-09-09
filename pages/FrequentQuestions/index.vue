2ea0861e24457ddf86e9ca9c0d9a91d85e68b9cd
Diego Reyes <diegorele13@gmail.com>
2022-09-08 19:27:07 -0500

    done: frequentQuestions page

---
+++ pages/FrequentQuestions/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="w-full">
+        <Inquiries />
+    </div>
+</template>
+
+<script>
+import Inquiries from './components/inquiries.vue'
+export default {
+    components: { Inquiries },
+}
+</script>
