f38a57ffa0650f7004bef5b416cb3bdf18125fda
Diego Reyes <diegorele13@gmail.com>
2022-09-09 16:40:43 -0500

    linked: questions with store

--- components/FrequentQuestions/Questions.vue
+++ components/FrequentQuestions/Questions.vue
@@ -17,7 +17,7 @@
         >
             <div
                 class="flex w-full sm:w-5/12"
-                v-for="(question, index) in questions"
+                v-for="(question, index) in getFrequentQuestion"
                 :key="index"
             >
                 <Question :question="question" />
@@ -27,29 +27,13 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import Icons from '../global/Icons.vue'
 import Question from './Question.vue'
 export default {
     components: { Icons, Question },
-    data: () => ({
-        questions: [
-            {
-                question: '¿Necesitas trasladar tu servicio a otro lugar?',
-                answer: 'El costo aproximado de traslado es de $80 + IVA. Si tienes más de 12 meses con nosotros podrás acceder a un traslado sin costo por una sola ocasión.Para solicitar el traslado puedes hacerlo ingresando a nuestro Portal de autoservicio, al correo atencionalcliente@puntonet.ec, desde nuestro Asistente Virtual CELTY al WhatsApp +593 980 828 349. Por favor especifica claramente la dirección, numeración y referencias, si cuentas con las coordenadas exactas nos ayudarías mucho con el proceso. Celerity EC, te entrega una cantidad de 350 metros de cable para la instalación en tu nuevo domicilio si tu plan es de Fibra, si la distancia es mayor, la instalación no será posible.',
-            },
-            {
-                question: '¿Necesitas trasladar tu servicio a otro lugar?',
-                answer: 'El costo aproximado de traslado es de $80 + IVA. Si tienes más de 12 meses con nosotros podrás acceder a un traslado sin costo por una sola ocasión.Para solicitar el traslado puedes hacerlo ingresando a nuestro Portal de autoservicio, al correo atencionalcliente@puntonet.ec, desde nuestro Asistente Virtual CELTY al WhatsApp +593 980 828 349. Por favor especifica claramente la dirección, numeración y referencias, si cuentas con las coordenadas exactas nos ayudarías mucho con el proceso. Celerity EC, te entrega una cantidad de 350 metros de cable para la instalación en tu nuevo domicilio si tu plan es de Fibra, si la distancia es mayor, la instalación no será posible.',
-            },
-            {
-                question: '¿Necesitas trasladar tu servicio a otro lugar?',
-                answer: 'El costo aproximado de traslado es de $80 + IVA. Si tienes más de 12 meses con nosotros podrás acceder a un traslado sin costo por una sola ocasión.Para solicitar el traslado puedes hacerlo ingresando a nuestro Portal de autoservicio, al correo atencionalcliente@puntonet.ec, desde nuestro Asistente Virtual CELTY al WhatsApp +593 980 828 349. Por favor especifica claramente la dirección, numeración y referencias, si cuentas con las coordenadas exactas nos ayudarías mucho con el proceso. Celerity EC, te entrega una cantidad de 350 metros de cable para la instalación en tu nuevo domicilio si tu plan es de Fibra, si la distancia es mayor, la instalación no será posible.',
-            },
-            {
-                question: '¿Necesitas trasladar tu servicio a otro lugar?',
-                answer: 'El costo aproximado de traslado es de $80 + IVA. Si tienes más de 12 meses con nosotros podrás acceder a un traslado sin costo por una sola ocasión.Para solicitar el traslado puedes hacerlo ingresando a nuestro Portal de autoservicio, al correo atencionalcliente@puntonet.ec, desde nuestro Asistente Virtual CELTY al WhatsApp +593 980 828 349. Por favor especifica claramente la dirección, numeración y referencias, si cuentas con las coordenadas exactas nos ayudarías mucho con el proceso. Celerity EC, te entrega una cantidad de 350 metros de cable para la instalación en tu nuevo domicilio si tu plan es de Fibra, si la distancia es mayor, la instalación no será posible.',
-            },
-        ],
-    }),
+    computed: {
+        ...mapGetters('frequentQuestion', ['getFrequentQuestion']),
+    },
 }
 </script>
