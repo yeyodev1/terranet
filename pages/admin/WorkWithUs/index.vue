d33113747a4e3c0938a518d9ac766157140edd7c
Luis Reyes <lualreye@gmail.com>
2022-10-24 08:24:10 -0500

    feat: vacancy view

--- pages/admin/WorkWithUs/index.vue
+++ pages/admin/WorkWithUs/index.vue
@@ -1,86 +1,123 @@
 <template>
   <app-layout>
-    <template v-slot:slotA>
-      <app-title title="Trabaja con nosotros" />
-    </template>
-    <template v-slot:slotB>
-      <div class="border border-lightBlue rounded-lg mt-6 p-6">
-        <button 
-          class="w-full flex justify-between items-center"
-          @click="showCard">
-          <p class="text-lg font-bold font-primary text-white">
-            Crear Vacante
-          </p>
-          <div class="w-5 h-5 flex justify-center items-center">
-            <icons
-              :name="arrowDisplay" class="text-yellow font-principal"/>
+    <app-title title="Trabaja con nosotros" />
+    <div class="border border-lightBlue rounded-lg mt-6 p-6">
+      <button
+        class="w-full flex justify-between items-center"
+        @click="showCard"
+      >
+        <p class="text-lg font-bold font-primary text-white">Crear Vacante</p>
+        <div class="w-5 h-5 flex justify-center items-center">
+          <icons :name="arrowDisplay" class="text-yellow font-principal" />
+        </div>
+      </button>
+      <div v-if="isOpen" class="w-full flex flex-col justify-start mt-6">
+        <div class="w-full flex flex-wrap justify-between items-center pl-1">
+          <div class="w-full flex justify-start items-start">
+            <label class="text-white font-primary text-lg mr-1">Título: </label>
+            <input
+              v-model="jobTitle"
+              placeholder="Ingresa el nombre"
+              type="text"
+              class="
+                w-full
+                text-white
+                outline-none
+                border-b border-lightBlue
+                px-1
+                bg-appBackground
+              "
+            />
           </div>
-        </button>
-        <div v-if="isOpen" class="w-full flex flex-col justify-start mt-6">
-          <div class="w-full flex flex-wrap justify-between items-center pl-1">
-            <div class="w-full flex justify-start items-start">
-              <label class="text-white font-primary text-lg mr-1">Título: </label>
-              <input 
-                v-model="jobTitle" 
-                placeholder="Ingresa el nombre"
-                type="text"
-                class="w-full text-white outline-none border-b border-lightBlue px-1 bg-appBackground">
-            </div>
-            <div class="w-full lg:w-1/2 flex flex-col justify-start items-start mt-4">
-              <label class="text-white font-primary text-lg mr-1">Descripción: </label>
-              <textarea 
-                v-model="jobDescription" 
-                placeholder="Descripcion"
-                type="text"
-                class="w-full lg:w-3/4 text-white outline-none border-b border-lightBlue px-1 bg-appBackground" />
-            </div>
-            <div class="w-full lg:w-1/2 flex flex-col justify-start items-start mt-4">
-              <label class="text-white font-primary text-lg mr-1">Requesitos: </label>
-              <input
-                v-model="jobRequirements"
-                @keyup.enter="getRequirement" 
-                placeholder="Requisito"
-                type="text"
-                class="w-full lg:w-3/4 text-white outline-none border-b border-lightBlue px-1 bg-appBackground mt-6" />
-            </div>
-            <div class="w-full flex justify-start items-baseline mt-3">
-              <p class="text-white mr-2">
-                Requisitos:
-              </p>
-              <div v-if="requirements.length" class="w-full flex flex-wrap mt-3">
-                <div
-                  v-for="(requirement, i) in requirements"
-                  :key="i"
-                  class="flex items-center bg-grey px-1 rounded-full mr-2 mt-2">
-                  <span
-                    class="text-appBackground mr-1"
-                    >{{ requirement }}
-                  </span>
-                  <button
-                    @click="removeRequirement(i)"
-                    class="rounded-full bg-red w-3 h-3">
-                    <icons name="close" class="text-white" />
-                  </button>
-                </div>
+          <div
+            class="w-full lg:w-1/2 flex flex-col justify-start items-start mt-4"
+          >
+            <label class="text-white font-primary text-lg mr-1"
+              >Descripción:
+            </label>
+            <textarea
+              v-model="jobDescription"
+              placeholder="Descripcion"
+              type="text"
+              class="
+                w-full
+                lg:w-3/4
+                text-white
+                outline-none
+                border-b border-lightBlue
+                px-1
+                bg-appBackground
+              "
+            />
+          </div>
+          <div
+            class="w-full lg:w-1/2 flex flex-col justify-start items-start mt-4"
+          >
+            <label class="text-white font-primary text-lg mr-1"
+              >Requesitos:
+            </label>
+            <input
+              v-model="jobRequirements"
+              @keyup.enter="getRequirement"
+              placeholder="Requisito"
+              type="text"
+              class="
+                w-full
+                lg:w-3/4
+                text-white
+                outline-none
+                border-b border-lightBlue
+                px-1
+                bg-appBackground
+                mt-6
+              "
+            />
+          </div>
+          <div class="w-full flex justify-start items-baseline mt-3">
+            <p class="text-white mr-2">Requisitos:</p>
+            <div v-if="requirements.length" class="w-full flex flex-wrap mt-3">
+              <div
+                v-for="(requirement, i) in requirements"
+                :key="i"
+                class="flex items-center bg-grey px-1 rounded-full mr-2 mt-2"
+              >
+                <span class="text-appBackground mr-1">{{ requirement }} </span>
+                <button
+                  @click="removeRequirement(i)"
+                  class="rounded-full bg-red w-3 h-3"
+                >
+                  <icons name="close" class="text-white" />
+                </button>
               </div>
             </div>
           </div>
-          <button
-            @click="saveVacancy"
-            class="w-28 rounded-lg py-1 px-2 flex justify-center items-center text-appBackground mt-6"
-            :class="{ 'bg-yellow': isFormValid, 'bg-grey': !isFormValid }">
-            Guardar
-          </button>
         </div>
+        <button
+          @click="saveVacancy"
+          class="
+            w-28
+            rounded-lg
+            py-1
+            px-2
+            flex
+            justify-center
+            items-center
+            text-appBackground
+            mt-6
+          "
+          :class="{ 'bg-yellow': isFormValid, 'bg-grey': !isFormValid }"
+        >
+          Guardar
+        </button>
       </div>
-    </template>
+    </div>
   </app-layout>
 </template>
 
 <script>
-import axios from 'axios'
+import { mapActions, mapGetters } from 'vuex'
 import icons from '@/components/global/Icons.vue'
-import AppTitle from '~/components/App/components/AppTitle.vue';
+import AppTitle from '~/components/App/components/AppTitle.vue'
 import AppLayout from '@/components/App/components/AppLayout.vue'
 
 export default {
@@ -88,7 +125,7 @@ export default {
   components: {
     icons,
     AppTitle,
-    AppLayout
+    AppLayout,
   },
   data: () => ({
     isOpen: false,
@@ -97,15 +134,19 @@ export default {
     jobRequirements: '',
     requirements: [],
   }),
-   computed: {
+  computed: {
+    ...mapGetters('vacancy', ['getVacancies']),
     arrowDisplay() {
-      return !this.isOpen ? 'arrowDown' : 'close';
+      return !this.isOpen ? 'arrowDown' : 'close'
     },
     isFormValid() {
-      return this.namePlan !== '' && this.pricePlan !== '' && this.speedPlan !== '';
-    }
+      return (
+        this.namePlan !== '' && this.pricePlan !== '' && this.speedPlan !== ''
+      )
+    },
   },
   methods: {
+    ...mapActions('vacancy', ['setVacancy', 'fetchVacancy']),
     showCard() {
       this.isOpen = !this.isOpen
     },
@@ -126,11 +167,11 @@ export default {
       const vacancy = {
         jobVacancy: this.jobTitle,
         jobDescription: this.jobDescription,
-        jobRequirements: this.requirements
+        jobRequirements: this.requirements,
       }
-      const response = await axios.post(`${process.env.NUXT_API}api/jobVacs`, vacancy)
+      this.setVacancy(vacancy)
       this.resetValues()
-    }
-  }
+    },
+  },
 }
 </script>
---
+++ store/vacancy.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+
+const state = () => ({
+  vacancies: []
+})
+
+const getters = {
+  getVacancies(state) {
+    return state.vacancies || []
+  },
+}
+
+const mutations = {
+  SET_VACANCIES(state, payload) {
+    state.vacancies = payload
+  },
+}
+
+const actions = {
+  async fetchVacancies({ commit }, payload) {
+    try {
+      const response = await axios.get(`${process.env.NUXT_API}api/jobVacs`,
+      {
+        headers: {
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+        }
+      })
+      commit('SET_VACANCIES', response.data.data)
+    } catch (e) {
+      console.error('CANNOT_GET_VACANCIES', e)
+    }
+  },
+  async setVacancy({ commit, dispatch }, payload) {
+    try {
+      const reponse = await axios.post(`${process.env.NUXT_API}api/jobVacs`,
+      payload,
+      {
+        headers: {
+            Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+        }
+      })
+      dispatch('workWithUs/fetchVacancies')
+    } catch (e) {
+      console.error('CANNOT_POST_VACANCY', e)
+    }
+  },
+}
+
+export default {
+    state,
+    getters,
+    mutations,
+    actions,
+}
