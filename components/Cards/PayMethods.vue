247bc5352037bd3a5050669ffbc7ac3621f7b716
Luis Reyes <lualreye@gmail.com>
2022-10-25 18:46:58 -0500

    fix: ways to pays

--- components/Cards/PayMethods.vue
+++ components/Cards/PayMethods.vue
@@ -1,142 +1,169 @@
 <template>
-    <div class="flex flex-col items-center justify-center w-full">
-        <div
-            class="flex flex-col items-center justify-center w-4/5 px-24 pt-8 mt-8 border-b md:justify-evenly max-w-7xl background pb-7"
-        >
-            <!-- PICHINCHA BANK  -->
-            <div
-                class="flex flex-col items-center justify-around w-full pb-6 border-b sm:flex-row border-lightBlue pt-7"
-            >
-                <div class="flex w-24 h-11 m">
-                    <img
-                        src="@/static/Banks/BancoPichincha.png"
-                        alt="Logo del banco pichincha"
-                    />
-                </div>
-                <div class="flex flex-col sm:flex-row">
-                    <div>
-                        <p
-                            class="font-semibold text-center text-white font-principal"
-                        >
-                            Banco Pichincha
-                        </p>
-                    </div>
-                </div>
-                <div>
-                    <p
-                        class="font-semibold text-center text-white font-principal"
-                    >
-                        Código: 74801
-                    </p>
-                </div>
-            </div>
-            <!-- BOLIVARIANO BANK -->
-            <div
-                class="flex flex-col items-center justify-around w-full pb-6 border-b sm:flex-row border-lightBlue pt-7"
-            >
-                <div class="flex w-24 h-11 m">
-                    <img
-                        src="@/static/Banks/BancoBolivarianoYWestern.png"
-                        alt="Logo del banco bolivariano"
-                    />
-                </div>
-                <div class="flex flex-col sm:flex-row">
-                    <div>
-                        <p
-                            class="font-semibold text-center text-white font-principal"
-                        >
-                            Banco Bolivariano <br />
-                            Cuenta Activa Western union
-                        </p>
-                        <p
-                            class="font-semibold text-center text-white font-principal"
-                        >
-                            Cuenta <br />
-                            Corriente: 0065061860
-                        </p>
-                    </div>
-                </div>
-                <div>
-                    <p
-                        class="font-semibold text-center text-white font-principal"
-                    >
-                        Cuenta corriente: 0065061860
-                    </p>
-                </div>
-            </div>
-            <!-- PRODUBANCO  -->
-            <div
-                class="flex flex-col items-center justify-around w-full pb-6 border-b sm:flex-row border-lightBlue pt-7"
-            >
-                <div class="flex w-24 h-11 m">
-                    <img
-                        src="@/static/Banks/ProdubancoYServipago.png"
-                        alt="Logo de Produbanco"
-                    />
-                </div>
-                <div class="flex flex-col sm:flex-row">
-                    <div>
-                        <p
-                            class="font-semibold text-center text-white font-principal"
-                        >
-                            Produbanco <br />
-                            Servipagos
-                        </p>
-                    </div>
-                </div>
-                <div>
-                    <p
-                        class="font-semibold text-center text-white font-principal"
-                    >
-                        Cuenta <br class="sm:hidden" />
-                        corriente: 02011019065
-                    </p>
-                </div>
-            </div>
-            <!-- BANCO DEL PACIFICO  -->
-            <div
-                class="flex flex-col items-center justify-around w-full pb-6 sm:flex-row pt-7"
-            >
-                <div class="flex w-24 h-11 m">
-                    <img src="@/static/Banks/BancoPacifico.png" alt="" />
-                </div>
-                <div class="flex flex-col sm:flex-row">
-                    <div>
-                        <p
-                            class="font-semibold text-center text-white font-principal"
-                        >
-                            Banco Del Pacífico
-                        </p>
-                    </div>
-                </div>
-                <div>
-                    <p
-                        class="font-semibold text-center text-white font-principal"
-                    >
-                        Cuenta ahorros: 1056182376
-                    </p>
-                    <p
-                        class="font-semibold text-center text-white font-principal"
-                    >
-                        Roxana Torres Terranova
-                    </p>
-                    <p
-                        class="font-semibold text-center text-white font-principal"
-                    >
-                        Ci. transferencia: 0915776264
-                    </p>
-                </div>
-            </div>
+  <div class="flex flex-col items-center justify-center w-full">
+    <div
+      class="
+        flex flex-col
+        items-center
+        justify-center
+        w-4/5
+        px-24
+        pt-8
+        mt-8
+        border-b
+        md:justify-evenly
+        max-w-7xl
+        background
+        pb-7
+      "
+    >
+      <!-- PICHINCHA BANK  -->
+      <div
+        class="
+          flex flex-col
+          items-center
+          justify-around
+          w-full
+          pb-6
+          border-b
+          sm:flex-row
+          border-lightBlue
+          pt-7
+        "
+      >
+        <div class="flex w-24 h-11 m">
+          <img
+            src="@/static/Banks/BancoPichincha.png"
+            alt="Logo del banco pichincha"
+          />
         </div>
-        <div class="w-4/5 mt-3 mb-10 max-w-7xl">
-            <p class="text-xs text-white font-principal sm:text-sm">
-                Envianos tu comprobante de pago por whatsapp
+        <div class="flex flex-col sm:flex-row">
+          <div>
+            <p class="font-semibold text-center text-white font-principal">
+              Banco Pichincha <br />
+              Corresponsales Mi Vecino
             </p>
-            <p class="text-xs text-white font-principal sm:text-sm">
-                Realiza el pago de tus planes cada mes
-            </p>
+          </div>
         </div>
+        <div>
+          <p class="font-semibold text-center text-white font-principal">
+            Código: 74801 <br />
+            + Número de Identificación del titular
+          </p>
+        </div>
+      </div>
+      <!-- BOLIVARIANO BANK -->
+      <div
+        class="
+          flex flex-col
+          items-center
+          justify-around
+          w-full
+          pb-6
+          border-b
+          sm:flex-row
+          border-lightBlue
+          pt-7
+        "
+      >
+        <div class="flex w-24 h-11 m">
+          <img
+            src="@/static/Banks/BancoBolivarianoYWestern.png"
+            alt="Logo del banco bolivariano"
+          />
+        </div>
+        <div class="flex flex-col sm:flex-row">
+          <div>
+            <p class="font-semibold text-center text-white font-principal">
+              Banco Bolivariano <br />
+              Cuenta Activa Western union
+            </p>
+          </div>
+        </div>
+        <div>
+          <p class="font-semibold text-center text-white font-principal">
+            Cuenta corriente: 0065061860
+          </p>
+        </div>
+      </div>
+      <!-- PRODUBANCO  -->
+      <div
+        class="
+          flex flex-col
+          items-center
+          justify-around
+          w-full
+          pb-6
+          border-b
+          sm:flex-row
+          border-lightBlue
+          pt-7
+        "
+      >
+        <div class="flex w-24 h-11 m">
+          <img
+            src="@/static/Banks/ProdubancoYServipago.png"
+            alt="Logo de Produbanco"
+          />
+        </div>
+        <div class="flex flex-col sm:flex-row">
+          <div>
+            <p class="font-semibold text-center text-white font-principal">
+              Produbanco <br />
+              Servipagos
+            </p>
+          </div>
+        </div>
+        <div>
+          <p class="font-semibold text-center text-white font-principal">
+            Cuenta <br class="sm:hidden" />
+            corriente: 02011019065
+          </p>
+        </div>
+      </div>
+      <!-- BANCO DEL PACIFICO  -->
+      <div
+        class="
+          flex flex-col
+          items-center
+          justify-around
+          w-full
+          pb-6
+          sm:flex-row
+          pt-7
+        "
+      >
+        <div class="flex w-24 h-11 m">
+          <img src="@/static/Banks/BancoPacifico.png" alt="" />
+        </div>
+        <div class="flex flex-col sm:flex-row">
+          <div>
+            <p class="font-semibold text-center text-white font-principal">
+              Banco Del Pacífico
+            </p>
+          </div>
+        </div>
+        <div>
+          <p class="font-semibold text-center text-white font-principal">
+            Cuenta ahorros: 1056182376
+          </p>
+          <p class="font-semibold text-center text-white font-principal">
+            Roxana Torres Terranova
+          </p>
+          <p class="font-semibold text-center text-white font-principal">
+            Ci. transferencia: 0915776264
+          </p>
+        </div>
+      </div>
     </div>
+    <div class="w-4/5 mt-3 mb-10 max-w-7xl">
+      <p class="text-xs text-white font-principal sm:text-sm">
+        Envianos tu comprobante de pago por whatsapp
+      </p>
+      <p class="text-xs text-white font-principal sm:text-sm">
+        Realiza el pago de tus planes cada mes
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -145,14 +172,14 @@ export default {}
 
 <style scoped>
 .background {
-    background: linear-gradient(
-        155.02deg,
-        rgba(250, 251, 255, 0.4) 0.55%,
-        rgba(250, 251, 255, 0.190022) 20.21%,
-        rgba(250, 251, 255, 0.124771) 34.02%,
-        rgba(250, 251, 255, 0.0654234) 44.23%,
-        rgba(250, 251, 255, 0.02) 75.4%
-    );
-    border-radius: 8px;
+  background: linear-gradient(
+    155.02deg,
+    rgba(250, 251, 255, 0.4) 0.55%,
+    rgba(250, 251, 255, 0.190022) 20.21%,
+    rgba(250, 251, 255, 0.124771) 34.02%,
+    rgba(250, 251, 255, 0.0654234) 44.23%,
+    rgba(250, 251, 255, 0.02) 75.4%
+  );
+  border-radius: 8px;
 }
 </style>
--- pages/WaysToPay/components/WaysToPay.vue
+++ pages/WaysToPay/components/WaysToPay.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="w-full">
-        <div class="background">
-            <p
-                class="pt-8 text-2xl font-bold text-center text-white font-principal sm:text-5xl"
-            >
-                Formas de pago
-            </p>
-            <p
-                class="text-base text-center text-white font-principal sm:text-3xl"
-            >
-                <strong class="text-lightBlue font-principal">Terranet</strong>
-                piensa en tu comodidad
-            </p>
-            <PayMethods />
-            <Benefits />
-            <PhoneCallToAction />
-        </div>
+  <div class="w-full">
+    <div class="background">
+      <p
+        class="
+          pt-8
+          text-2xl
+          font-bold
+          text-center text-white
+          font-principal
+          sm:text-5xl
+        "
+      >
+        Formas de pago
+      </p>
+      <p class="text-base text-center text-white font-principal sm:text-3xl">
+        <strong class="text-lightBlue font-principal">Terranet</strong>
+        piensa en tu comodidad
+      </p>
+      <PayMethods />
+      <Benefits />
+      <PhoneCallToAction />
     </div>
+  </div>
 </template>
 
 <script>
@@ -25,15 +30,15 @@ import PayMethods from '~/components/Cards/PayMethods.vue'
 import Benefits from '~/components/Home/Benefits.vue'
 import PhoneCallToAction from '~/components/Home/PhoneCallToAction.vue'
 export default {
-    components: { Header, PayMethods, Benefits, PhoneCallToAction },
+  components: { Header, PayMethods, Benefits, PhoneCallToAction },
 }
 </script>
 
 <style scoped>
 .background {
-    background-image: url('@/static/Backgrounds/background-4.png');
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
+  background-image: url('@/static/Backgrounds/background-4.png');
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
 }
 </style>
