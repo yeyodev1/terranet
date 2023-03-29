b0dec307466ad060e5ea40f0e170d22fb3815db9
Luis Reyes <lualreye@gmail.com>
2023-03-29 15:52:22 -0500

    fix: getting rules properly

--- components/global/The/Content/Footer/content.vue
+++ components/global/The/Content/Footer/content.vue
@@ -1,29 +1,24 @@
 <template>
-    <div class="flex flex-col items-center justify-start w-full pl-4 lg:px-5">
-        <p class="w-full text-xl font-bold text-blue">
-            {{ linkSection.title }}
-        </p>
-        <div class="flex flex-col w-full">
-            <nuxt-link
-                v-for="(link, index) in linkSection.links"
-                :key="index"
-                target="_blank"
-                :to="link.url"
-                class="hover:text-lightBlue w-full mt-3 text-base text-black transition-border"
-            >
-                {{ link.name }}
-            </nuxt-link>
-        </div>
+  <div class="flex flex-col items-center justify-start w-full pl-4 lg:px-5">
+    <p class="w-full text-xl font-bold text-blue">
+      {{ linkSection.title }}
+    </p>
+    <div class="flex flex-col w-full">
+      <nuxt-link v-for="(link, index) in linkSection.links" :key="index" target="_blank" :to="link.url"
+        class="hover:text-lightBlue w-full mt-3 text-base text-black transition-border">
+        {{ link.name }}
+      </nuxt-link>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: {
-        linkSection: {
-            type: Object,
-            required: true,
-        },
+  props: {
+    linkSection: {
+      type: Object,
+      required: true,
     },
+  },
 }
 </script>
