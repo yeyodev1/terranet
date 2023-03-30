e3d43b6cce9b7aff858d692af23a6bd53b816c2f
Diego Reyes <diegorele13@gmail.com>
2023-03-30 13:47:57 -0500

    fix: typo

--- pages/HomePlans.vue
+++ pages/HomePlans.vue
@@ -1,28 +1,26 @@
 <template>
-    <div class="background">
-        <div class="w-4/5 mx-auto max-w-7xl">
-            <p
-                class="mt-8 text-2xl font-bold text-center text-white lg:pt-20 sm:text-5xl"
-            >
-                Plan Hogar
-            </p>
-            <p class="text-base font-medium text-center text-white sm:text-2xl">
-                !Tenemos el plan que necesitas!
-            </p>
-            <div
-                class="flex flex-wrap items-center justify-between w-full max-w-7xl"
-            >
-                <div
-                    class="w-full mt-6 sm:w-2/5 sm:mt-12"
-                    v-for="(homePlan, index) in getPlans"
-                    :key="index"
-                >
-                    <Plan :homePlan="homePlan" />
-                </div>
-            </div>
+  <div class="background">
+    <div class="w-4/5 mx-auto max-w-7xl">
+      <p
+        class="mt-8 text-2xl font-bold text-center text-white lg:pt-20 sm:text-5xl"
+      >
+        Plan Hogar
+      </p>
+      <p class="text-base font-medium text-center text-white sm:text-2xl">
+        ¡Tenemos el plan que necesitas!
+      </p>
+      <div class="flex flex-wrap items-center justify-between w-full max-w-7xl">
+        <div
+          class="w-full mt-6 sm:w-2/5 sm:mt-12"
+          v-for="(homePlan, index) in getPlans"
+          :key="index"
+        >
+          <Plan :homePlan="homePlan" />
         </div>
-        <TestPlan />
+      </div>
     </div>
+    <TestPlan />
+  </div>
 </template>
 
 <script>
@@ -32,21 +30,21 @@ import Plan from '~/components/Plans/Plan.vue'
 import { mapGetters } from 'vuex'
 
 export default {
-    components: {
-        TestPlan,
-        Footer,
-        Plan,
-    },
-    computed: {
-        ...mapGetters('plans', ['getPlans']),
-    },
+  components: {
+    TestPlan,
+    Footer,
+    Plan,
+  },
+  computed: {
+    ...mapGetters('plans', ['getPlans']),
+  },
 }
 </script>
 
 <style scoped>
 @font-face {
-    font-family: 'FontJek';
-    font-display: swap;
-    src: url('@/assets/fonts/Fontjek.ttf') format('truetype');
+  font-family: 'FontJek';
+  font-display: swap;
+  src: url('@/assets/fonts/Fontjek.ttf') format('truetype');
 }
 </style>
