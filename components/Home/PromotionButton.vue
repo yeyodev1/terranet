fb16b43458ca3407580e5bee17e7c7bfb14482f5
Luis Reyes <lualreye@gmail.com>
2023-04-01 13:48:54 -0500

    fix: copy

--- components/Home/PromotionButton.vue
+++ components/Home/PromotionButton.vue
@@ -3,7 +3,7 @@
     <nuxt-link to="/hireService">
       <button
         class="text-xs font-principal font-semibold text-black bg-yellow px-3 py-2 rounded-md sm:px-6 sm:py-3 sm:text-base">
-        Ver promociones
+        Contrata ahora
       </button>
     </nuxt-link>
   </div>
