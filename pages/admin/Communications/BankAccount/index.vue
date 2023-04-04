4adfcb323674f50a33923be421d1c3cd8c57bdbb
Diego Reyes <diegorele13@gmail.com>
2023-04-04 02:48:44 -0500

    done: bank component

---
+++ pages/admin/Communications/BankAccount/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full">
+    <BankInputs />
+    <BankCard />
+  </div>
+</template>
+
+<script>
+import BankCard from './components/BankCard.vue'
+import BankInputs from './components/BankInputs.vue'
+export default {
+  components: {
+    BankCard,
+    BankInputs,
+  },
+  layout: 'app',
+}
+</script>
