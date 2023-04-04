11d44710a75f1ae81c8ff03c8c5f0c8a4da4f4c0
Diego Reyes <diegorele13@gmail.com>
2023-04-04 02:47:58 -0500

    done: bank detail components

---
+++ pages/admin/Communications/BankAccount/components/BankDetail.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="w-full flex justify-between p-5">
+    <div class="flex md:w-1/4 items-center justify-center">
+      <div>
+        <img :src="url" alt="Logo del banco pichincha" class="w-36 h-36" />
+      </div>
+    </div>
+    <div class="flex md:w-1/4 flex-col sm:flex-row items-center justify-center">
+      <div>
+        <p class="font-semibold text-center text-white font-principal">
+          {{ name }}
+        </p>
+      </div>
+    </div>
+    <div class="flex md:w-1/4 flex-col sm:flex-row items-center justify-center">
+      <div>
+        <p class="font-semibold text-center text-white font-principal">
+          {{ details }}
+        </p>
+      </div>
+    </div>
+    <button class="text-red" @click="deleteBankAccount">Borrar</button>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    details: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    ...mapActions('banks', ['deleteBank']),
+    deleteBankAccount() {
+      this.deleteBank(this.id)
+    },
+  },
+}
+</script>
